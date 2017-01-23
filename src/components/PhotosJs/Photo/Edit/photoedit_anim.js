667d4ee1d4ac021c33c0ccd14454a2677651fd41
Mark <web.markyn@gmail.com>
2017-01-23 17:57:39 +0300

    testing 1

---
+++ src/components/PhotosJs/Photo/Edit/photoedit_anim.js
@@ -0,0 +1,13 @@
+const likeAnim = (function() {
+    let anim = (className) => {
+        
+    }
+    
+    return {
+        anim: function(className) {
+            anim(className);
+        }
+    }
+})();
+
+module.exports = likeAnim;
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -4,9 +4,9 @@ import Like from './../../components/Like/Like';
 import AlbumHeaderEdit from '../../components/AlbumHeaderEdit/AlbumHeaderEdit';
 import MainHeaderEdit from '../../components/MainHeaderEdit/MainHeaderEdit';
 import AlbumEdit from './../../components/AlbumEdit/AlbumEdit';
-import PhotoEdit from './../../components/Photos/Photo/Edit/PhotoEdit';
+import PhotoEdit from './../../components/PhotosJs/Photo/Edit/PhotoEdit';
 import AddAlbum from './../../components/AddAlbum/AddAlbum';
-import Photos from './../../components/Photos/Photos.jsx';
+import Photos from './../../components/PhotosJs/Photos.jsx';
 
 import s from './search.scss';
 
